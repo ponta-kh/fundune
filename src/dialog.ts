293498ddcc1b06605b6cf5d515260346d9df0ec1
ponta-kh <kei.dev.itwork@gmail.com>
2025-10-04 19:19:51 +0900

    merge feature/npm-upload
    * npm公開準備
    
    * README初版
    
    * 修正
    
    * 修正
    
    * 修正
    
    * feat: npm公開の準備
    
    npmでライブラリを公開するための準備作業。
    
    - package.jsonにライセンスとリポジトリ情報を追加
    
    - README.mdをプロジェクトに合わせて更新
    
    - MITライセンスファイルを追加
    
    - ESLint, Geminiの設定を更新

---
+++ src/dialog.ts
@@ -0,0 +1,3 @@
+/* ダイアログ用コンポーネント */
+export * from "./components/common/dialog/alert-dialog";
+export * from "./components/common/dialog/view-dialog";
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./components/common/alert";
+export * from "./components/common/card";
+export * from "./components/common/multiple-accordion";
+export * from "./components/common/table";
+export * from "./components/common/tooltip";
